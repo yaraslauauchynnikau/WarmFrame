8be22feb825f0ee087c51f356a9c986f32bede9a
YO <yaraslauauchynnikau@gmail.com>
2026-01-16 01:29:28 +0300

    Fixed Logger to actually have a queue

--- src/framework/LogManager.js
+++ src/framework/LogManager.js
@@ -6,23 +6,34 @@ const { EventTypes } = require('./Constants');
 const logPath = path.join(__dirname, '../../database/eventlog.json');
 
 class LogManager {
+    static #writeQueue = Promise.resolve();
+
     static init() {
-        eventBus.on(EventTypes.REPO_ACTION, LogManager.log);
-        eventBus.on(EventTypes.ERROR, LogManager.log);
+        eventBus.on(EventTypes.REPO_ACTION, (ev) => LogManager.log(ev));
+        eventBus.on(EventTypes.ERROR, (ev) => LogManager.log(ev));
     }
 
     static async log(event) {
-        try {
-            const data = await fs.readFile(logPath, 'utf-8')
-                .catch(() => '[]');
-            
-            const logs = JSON.parse(data);
-            logs.push(event);
-            await fs.writeFile(logPath, JSON.stringify(logs, null, 2));
-        }
-        catch (e) {
-            console.error('LOG ERROR', e);
-        }
+        this.#writeQueue = this.#writeQueue.then(async () => {
+            try {
+                let logs = [];
+                try {
+                    const data = await fs.readFile(logPath, 'utf-8');
+                    logs = data ? JSON.parse(data) : [];
+                } catch (e) {
+                    logs = [];
+                }
+
+                logs.push(event);
+
+                await fs.writeFile(logPath, JSON.stringify(logs, null, 2), 'utf-8');
+            }
+            catch (e) {
+                console.error('CRITICAL LOG ERROR:', e);
+            }
+        });
+
+        return this.#writeQueue;
     }
 }
 
